e5b36d0dd7bb744c0b4932943d4f03db82ee5288
tainaraPimentel <tainara_pimentel@outlook.com.br>
2025-01-31 10:56:37 -0300

    criação função adicionarAmigo

--- app.js
+++ app.js
@@ -4,3 +4,22 @@
 
 //lista que armazena os nomes digitados pelo usuário
 let listaDeAmigos = [];
+
+//função atrelada ao botão adicionar, armazena os valores digitados no input na lista de amigos
+function adicionarAmigo() {
+
+    let nome = document.querySelector('input').value;
+    console.log("botão adicionar foi clicado");
+    //validação de valor digitado pelo usuário 
+    if (nome == ""){
+        alert("erro: Por favor, insira um nome.");
+        console.log("Erro: Por favor, insira um nome.");
+    } else {
+        listaDeAmigos.push(nome);
+        //lista o nome inserido abaixo do campo
+        ordenarListaDeAmigo(listaDeAmigos);
+        //limpar o campo
+        limparCampo();
+        console.log("OK: Nome digitado inserido a lista e campo de digitação limpo");
+    }   
+}
