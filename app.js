3ec51169944bb7e8a78b7c4062e99fe4a4161763
tainaraPimentel <tainara_pimentel@outlook.com.br>
2025-01-31 10:52:40 -0300

    criação array listaDeAmigos

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-/*
-O principal objetivo deste desafio é fortalecer suas habilidades em 
-lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-*/
+//O principal objetivo deste desafio é fortalecer suas habilidades em 
+//lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
+
+//lista que armazena os nomes digitados pelo usuário
+let listaDeAmigos = [];
